6768bda935aadbd5cc3d17c4247608158220598f
nhope <nhope@ryerson.ca>
2021-05-28 21:44:59 -0400

    update

--- src/components/details.js
+++ src/components/details.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import {Header} from './layers'
+import {Header, Ingredient} from './layers'
 
-const URL ='http://watchfit.com/wp-content/uploads/2015/05/junk-food-and-its-effects_11.jpg';
-const ingrd = [
-                '1 ½ teaspoons salt', '1 teaspoon paprika',
-                '1 teaspoon ground cardamom', '1 teaspoon ground coriander',
-                '½ teaspoon ground black pepper', '¼ cup grapeseed oil',
-                '2 tablespoons maple syrup',
-                '1 (2 pound) salmon fillet, cut into 3-inch pieces'
-              ];
+const direct = [
+                'Stir salt, paprika, cardamom, coriander, and black pepper together in a bowl.\
+                 Add oil and maple syrup and stir until evenly combined.',
+                 'Preheat a non-stick frying pan over medium-high heat, about 350 \
+                 degrees F (175 degrees C).',
+                 'Dredge salmon pieces through the maple syrup mixture until evenly \
+                 coated on all sides.',
+                 'Cook salmon in the preheated pan until fish flakes easily with a \
+                 fork, 5 to 7 minutes per side.'
+]
 
 export default class Details extends Component {
   static propTypes = {
@@ -26,29 +28,26 @@ export default class Details extends Component {
           <Header />
 
           {/* Ingredients Layer */}
-          <div id={'ingredients'} className={'d-flex  row '}>
-            {/* Ingredient list */}
-            <div className={'col-12 col-lg-7 border border-primary '} >
-              <div className={'fw-bold'}>Ingredients</div >
-              <ul className={'list-unstyled'} >
-                {ingrd.map((item, index)=>{
-                  return(<li className={'p-1'} key={index}>{item}</li>)
-                })}
-              </ul >
-            </div >
-            {/* Image */}
-            <div className={'col-12 col-lg-5 border border-primary'} >
-              <img id={'meal'} src={URL} alt={'Completed meal '} />
-            </div >
-
-
-          </div >
+          <Ingredient />
 
           {/* Direction */}
           <div id={'direction'} className={'row'}>
-            {/* Image */}
-            <div className={'col-12 border border-primary '} >
 
+            {/* Directions */}
+            <div className={'col-12 border border-primary  py-3'} >
+              <h6 className={'fw-bold'}>Direction</h6 >
+                <dl >
+                  {direct.map((item, index)=>{
+                    return(
+                      <Fragment >
+                        <dt className={'px-1'} key={`${index + 't'}`} >{`Step ${index + 1} `} </dt >
+                        <dd className={'px-1'} key={`${index + 'd'}`} > {item } </dd>
+                      </Fragment >
+                    );
+                  })
+                }
+
+                </dl >
             </div >
 
 
--- src/components/layers.js
+++ src/components/layers.js
@@ -2,6 +2,16 @@ import React from 'react'
 import edit from './../assets/editdocument.png'
 import deletePic from './../assets/trash_bin2.png'
 
+const URL ='http://watchfit.com/wp-content/uploads/2015/05/junk-food-and-its-effects_11.jpg';
+const ingrd = [
+                '1 ½ teaspoons salt', '1 teaspoon paprika',
+                '1 teaspoon ground cardamom', '1 teaspoon ground coriander',
+                '½ teaspoon ground black pepper', '¼ cup grapeseed oil',
+                '2 tablespoons maple syrup',
+                '1 (2 pound) salmon fillet, cut into 3-inch pieces'
+              ];
+
+
 export const Header = props => {
   return(
     <div className={'row'}>
@@ -18,6 +28,30 @@ export const Header = props => {
           <img className={'detail-icon'} src={deletePic} alt={'Delete button'} />
         </button >
       </div >
+    </div >
+  )
+}
+
+
+export const Ingredient =  props => {
+  return(
+    <div id={'ingredients'} className={'d-flex flex-sm-row-reverse row py-3'}>
+
+      {/* Image */}
+      <div className={'d-flex flex-row align-items-center col-12 col-lg-5 border border-primary'} >
+        <img id={'meal'} src={URL} alt={'Completed meal '} />
+      </div >
+
+      {/* Ingredient list */}
+      <div className={'col-12 col-lg-7 border border-primary py-3'} >
+        <h6 className={'fw-bold'}>Ingredients</h6 >
+        <ul className={'list-unstyled'} >
+          {ingrd.map((item, index)=>{
+            return(<li className={'p-1'} key={index}>{item}</li>)
+          })}
+        </ul >
+      </div >
+
     </div >
   )
 }
