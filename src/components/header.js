9d839832f736a0ef16ad68f99e17bdcb1ff77a71
nhope <nhope@ryerson.ca>
2021-06-04 11:06:19 -0400

    update

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux'
 
 import edit from './../assets/editdocument1.png'
 import deletePic from './../assets/trash_bin2.png'
+import pdfPic from './../assets/pdf_download.png'
 import DeleteRecipe from './delete'
 
 import {openDeleteModal} from '../redux/slice'
@@ -21,16 +22,24 @@ class Header extends Component {
     return(
       <div className={'row'}>
         {/* Label */}
-        <div className={'col-9 col-sm-10'}>
+        <div className={'col-12 col-sm-10'}>
           <h3 className={'text-center'} >{this.props.data.title}</h3>
         </div >
         {/* Eidt buttons */}
-        <div className={'col-3 col-sm-2 d-flex flex-row'} >
-          <button type={'button'} className={'btn rounded-circle p-2 '} onClick={()=>{
+        <div id={'icon-buttons'} className={'col-1 col-sm-2 d-flex flex-row flex-wrap justify-content-evenly'} >
+
+
+          <button type={'button'} className={'btn rounded-circle p-1 '} disabled onClick={()=>{
+            this.props.openEditRecipe(this.props.data)}} >
+            <img className={'icon'} src={pdfPic} alt={'Pdf button'} title={'Download Pdf Version'}/>
+          </button >
+
+
+          <button type={'button'} className={'btn rounded-circle p-1 '} onClick={()=>{
             this.props.openEditRecipe(this.props.data)}} >
             <img className={'detail-icon'} src={edit} alt={'Edit button'} title={'Edit Recipe'}/>
           </button >
-          <button type={'button'} className={'btn rounded-circle p-2'} onClick={this.props.openDeleteModal} >
+          <button type={'button'} className={'btn rounded-circle p-1'} onClick={this.props.openDeleteModal} >
             <img className={'detail-icon'} src={deletePic} alt={'Delete button'} title={'Delete Recipe'}/>
           </button >
           {/* Modal */}
