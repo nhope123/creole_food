43364420ee34751d1728e69102f4c59de393ee06
nhope <nhope@ryerson.ca>
2021-06-07 23:11:06 -0400

    update

--- src/redux/pdf-load.js
+++ src/redux/pdf-load.js
@@ -2,7 +2,20 @@ import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-const path = require('path');
+// Layout styles for the pdf
+let pdfStyles = {
+  header:{ fontSize: 20, bold: true, alignment: 'center', },
+  subTitle:{ fontSize: 14, bold: true, alignment: 'left', margin: [0,5,0,0] },
+  steps:{ bold: true, margin: [0,3], },
+  context:{ margin:[10,0,0,0], },
+  pagecount:{ alignment: 'right', margin: [0,0,20,10], },
+  headerLink:{ alignment: 'right', margin: [0,10,10,0], fontSize: 9, italics: true, },
+};
+
+/*
+  getBase64ImageFromURL convert image from Url to base 64 image
+  url: String parameter of image location
+*/
 
 const getBase64ImageFromURL = (url) => {
   return new Promise((resolve, reject) => {
@@ -30,27 +43,38 @@ const getBase64ImageFromURL = (url) => {
   });
 }
 
+/*
+  Compile recipe data into pdf life for local download.
+  recipe: An object paramter containing a title, servingSize, ingredients,
+  directions, src, notes.
+*/
 
 export const pdfProcessing = async (recipe) =>{
-    let image = '';
-    getBase64ImageFromURL(recipe.src).then(value => {
-      image = {image: value, width: 250,}
-    }).catch(err => {
-      alert('Error adding image - Pdf will not contain recipe image!')
-    });
 
+  // Convert url to base 64 image or throw and alert message
+  let image = '';
+  getBase64ImageFromURL(recipe.src).then(value => {
+    image = {image: value, width: 250,}
+  }).catch(err => {
+    alert('Error adding image - Pdf will not contain recipe image!')
+  });
+
+  // Prepare the list of ingredients
   const ingredients =  recipe.ingredients.map((item) => {
     return ({ text: item, listType: 'none' });
   });
 
+  // Prepare the list of steps and directions
   const direction = [];
   recipe.directions.forEach((item,i) => {
     direction.push({text:`Step ${i+1}`,style: 'steps'})
     direction.push({text:item,style: 'context'})
   });
 
+  // Prepare any notes if available
   const note = (recipe.notes)? [{text:'**Note', style: 'steps'},{text: recipe.notes,style: 'context'}] : {};
 
+  // Organize the layout for the pdf document
   let pdfDefination = {
     footer:function(currentPage, pageCount) {
       return [
@@ -85,37 +109,7 @@ export const pdfProcessing = async (recipe) =>{
       direction,
       note,
     ],
-    styles:{
-      header:{
-        fontSize: 20,
-        bold: true,
-        alignment: 'center',
-      },
-      subTitle:{
-        fontSize: 14,
-        bold: true,
-        alignment: 'left',
-        margin: [0,5,0,0]
-
-      },
-      steps:{
-        bold: true,
-        margin: [0,3],
-      },
-      context:{
-        margin:[10,0,0,0],
-      },
-      pagecount:{
-        alignment: 'right',
-        margin: [0,0,20,10],
-      },
-      headerLink:{
-        alignment: 'right',
-        margin: [0,10,10,0],
-        fontSize: 9,
-        italics: true,
-      },
-    }
+    styles: pdfStyles,
   }
 
   // need Promise
