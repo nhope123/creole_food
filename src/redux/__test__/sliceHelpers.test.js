7196ed2e8ba80799bd8fb847dc8d3208f6180afb
nhope <nhope@ryerson.ca>
2021-07-03 22:16:49 -0400

    Jest testing

---
+++ src/redux/__test__/sliceHelpers.test.js
@@ -0,0 +1,29 @@
+import {
+  getRecipeTitles,
+  getLocal,
+  uniqueRecipes,
+  deleteFromLocal,
+  deleteRecipe,
+  addRecipe,
+}
+from '../sliceHelpers';
+import {recipes} from '../recipes';
+
+describe('getRecipeTitles()', function () {
+  test("Get list of recipe titles", () => {
+    let recipeList = [
+      'PELAU', 'CURRIED GOAT', 'CURRIED CHICKEN','STEWED CHICKEN',
+      'OVEN BARBECUED CHICKEN','SHEPHERD\'S PIE'
+    ];
+    expect(getRecipeTitles(recipes)).toEqual(recipeList);
+  });
+});
+
+describe('deleteRecipe()', function () {
+  test("Remove PELAU from recipe list", () => {
+    expect(deleteRecipe(recipes,'PELAU')).not.toContain(recipes[0]);
+  });
+  test("Remove CURRIED GOAT from recipe list", () => {
+    expect(deleteRecipe(recipes,'CURRIED GOAT')).not.toContain(recipes[1]);
+  });
+});
